80028f9009d3eccd7c2fb8675ea7e9133e626551
Andrew Kemp <andrewkemp70@gmail.com>
2013-05-05 14:38:46 +1000

    Added static pages controller, pages and stylesheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# defines the full title
+	def full_title(page_title)
+		base_title = "Andrew Kemp's Rails Notes"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
